c88185f1622853e6bd3e78a955e32875cbaf2741
Andrew Watts <wattsmart@gmail.com>
2026-07-11 00:28:47 +0100

    Quick update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/teacherkaren/', // Tells Vite to serve the app from this subfolder
+      base: '/', // Tells Vite to serve the app from this subfolder
       server: {
         port: 3000,
         host: '0.0.0.0',
